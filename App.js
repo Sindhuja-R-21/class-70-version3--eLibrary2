2fd1306dc10071c522433c26a7166182a0b4c7ec
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2022-04-13 19:45:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
+import * as Font from "expo-font";
+
+import BottomTabNavigator from "./components/BottomTabNavigator";
+
+export default class App extends Component {
+  render() { 
+      return <BottomTabNavigator />;  
+  }
+}
